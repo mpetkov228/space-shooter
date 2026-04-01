da93c8c070f2d0b71a48fa39461d717b384284f2
MihailPetkov96 <mpetkov228@gmail.com>
2026-04-01 19:39:16 +0300

    Add ability to shoot projectile to player

--- index.js
+++ index.js
@@ -11,12 +11,50 @@ window.addEventListener("keydown", (event) => {
     if (code === "KeyD") left += 10;
     if (code === "KeyW") top -= 10;
     if (code === "KeyS") top += 10;
-
+    
     if (left < 0) return;
     if (left + player.offsetWidth > gameScreen.offsetWidth) return;
-    if (top - (player.offsetHeight / 2) < 0) return;
-    if (top + (player.offsetHeight * 1.5) > gameScreen.offsetHeight) return;
+    if (top < 0) return;
+    if (top + player.clientHeight > gameScreen.offsetHeight) return;
 
     player.style.top = `${top}px`;
     player.style.left = `${left}px`;
 });
+
+
+window.addEventListener("mousedown", (event) => {
+    const projectile = createProjectile();
+    gameScreen.appendChild(projectile);
+    let start;
+
+    function moveProjectile(timestamp) {
+        if (start === undefined) {
+            start = timestamp;
+        }
+        const elapsed = timestamp - start;
+
+        const shift = Math.max(-0.5 * elapsed, -1000);
+        projectile.style.transform = `translateY(${shift}px)`;
+        if (shift > -800) {
+            requestAnimationFrame(moveProjectile)
+        } else {
+            gameScreen.removeChild(projectile);
+        }
+        console.log(shift);
+    }
+
+
+    requestAnimationFrame(moveProjectile);
+});
+
+function createProjectile() {
+    const playerPos = player.getBoundingClientRect();
+    const projectile = document.createElement("img");
+    projectile.src = "assets/projectile.png";
+    projectile.classList.add("projectile");
+    projectile.style.top = `${playerPos.top + 10}px`;
+    projectile.style.left = `${playerPos.left + 45}px`;
+
+    return projectile;
+}
+
