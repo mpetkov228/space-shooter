5f9da6e2c6f7143142f8cfdcbb741d7640af17a0
MihailPetkov96 <mpetkov228@gmail.com>
2026-04-03 19:58:26 +0300

    Implement player movement in bounds of screen

--- src/main.js
+++ src/main.js
@@ -1,35 +1,87 @@
-import { Application, Assets, Sprite } from "pixi.js";
+import { Application, Assets, Container, Sprite, UPDATE_PRIORITY } from 'pixi.js';
+
+const app = new Application();
+
+const keys = {};
+
+document.addEventListener("keydown", (event) => {
+  keys[event.code] = true;
+});
+
+document.addEventListener("keyup", (event) => {
+  keys[event.code] = false;
+});
+
+function move(player, time) {
+  if (keys["KeyA"] && player.x > 0) player.x -= 2.5 * time.deltaTime;
+  if (keys["KeyD"] && player.x + player.width < app.screen.width) player.x += 2.5 * time.deltaTime;
+  if (keys["KeyW"] && player.y > 0) player.y -= 2.5 * time.deltaTime;
+  if (keys["KeyS"] && player.y + player.height < app.screen.height) player.y += 2.5 * time.deltaTime;
+}
+
+async function createEnemy() {
+  const texture = await Assets.load('assets/enemy.png');
+  const enemy = new Sprite(texture);
+  enemy.cullable = true;
+  enemy.x = Math.floor(Math.random() * ((app.screen.width - enemy.width) - enemy.width) + enemy.width);
+  enemy.y = -enemy.height;
+
+  return enemy;
+}
 
 (async () => {
-  // Create a new application
-  const app = new Application();
+  await app.init({ background: '#1099bb', resizeTo: window });
 
-  // Initialize the application
-  await app.init({ background: "#1099bb", resizeTo: window });
+  document.getElementById('pixi-container').appendChild(app.canvas);
 
-  // Append the application canvas to the document body
-  document.getElementById("pixi-container").appendChild(app.canvas);
+  const container = new Container();
 
-  // Load the bunny texture
-  const texture = await Assets.load("/assets/bunny.png");
+  app.stage.addChild(container);
 
-  // Create a bunny Sprite
-  const bunny = new Sprite(texture);
+  const enemies = [];
 
-  // Center the sprite's anchor point
-  bunny.anchor.set(0.5);
+  // player
+  const texture = await Assets.load('assets/player.png');
 
-  // Move the sprite to the center of the screen
-  bunny.position.set(app.screen.width / 2, app.screen.height / 2);
+  const ship = new Sprite(texture);
+  const ratio = ship.height / ship.width;
+  ship.setSize(85, 85 * ratio);
 
-  // Add the bunny to the stage
-  app.stage.addChild(bunny);
+  ship.x = app.screen.width * 0.5;
+  ship.y = app.screen.height * 0.8;
 
-  // Listen for animate update
+  container.addChild(ship);
+
+  // enemy
+  const texture2 = await Assets.load('assets/enemy.png');
+
+  const enemy = new Sprite(texture2);
+  enemy.cullable = true;
+
+  container.addChild(enemy);
+
+  enemy.x = app.screen.width * 0.2;
+  enemy.y = -enemy.width;
+
+
+  const enemy2 = new Sprite(texture2);
+  enemy2.cullable = true;
+
+  container.addChild(enemy2);
+
+  enemy2.x = app.screen.width * 0.8;
+  enemy2.y = -enemy2.width;
+
   app.ticker.add((time) => {
-    // Just for fun, let's rotate mr rabbit a little.
-    // * Delta is 1 if running at 100% performance *
-    // * Creates frame-independent transformation *
-    bunny.rotation += 0.1 * time.deltaTime;
+    console.log(time.elapsedMS);
+    // container.addChild(await createEnemy());
+  });
+
+  app.ticker.add((time) => {
+    [enemy, enemy2].forEach(e => e.y += 1 * time.deltaTime);
+  });
+
+  app.ticker.add((time) => {
+    move(ship, time);
   });
 })();
